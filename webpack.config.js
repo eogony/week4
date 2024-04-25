685b8b65e91b37305ad10210455651ebdb535961
Elizabeth Ogony <eogony@gmail.com>
2024-04-25 22:25:25 +0300

    added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,6 +9,13 @@ module.exports = {
     filename: 'bundle.js',
     publicPath: '/dist/'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Week 4 Project',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
   module: {
     rules: [
       {
